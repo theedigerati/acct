7246089156750b77e11aa1aed9d667dcab901197
theedigerati <39467790+theedigerati@users.noreply.github.com>
2025-02-06 23:54:30 +0100

    add celery logging

--- apps/organisation/tasks.py
+++ apps/organisation/tasks.py
@@ -1,8 +1,10 @@
+import logging
 from celery import shared_task
 from celery.utils.log import get_task_logger
 from django.apps import apps
 
-logging = get_task_logger(__name__)
+logger = get_task_logger(__name__)
+logger.setLevel(logging.INFO)
 
 
 @shared_task(bind=True, retry_backoff=3, retry_kwargs={"max_retries": 5})
@@ -11,14 +13,14 @@ def setup_org_tenancy(self, org_id, request_user_id):
     org_object = Organisation.objects.get(id=int(org_id))
 
     if self.request.retries > 0:
-        logging.info(
+        logger.info(
             "[Task Retry] Attempt %d/%d",
             self.request.retries,
             self.retry_kwargs["max_retries"],
         )
 
-    logging.info("[Started] Creating Tenant for %s ...", org_object.name)
+    logger.info("[Started] Creating Tenant for %s ...", org_object.name)
     org_object.create_tenant(request_user_id)
-    logging.info("[Started] Creating default Financial Accounts")
+    logger.info("[Started] Creating default Financial Accounts")
     org_object.setup_default_accounts()
-    logging.info("[Finished] Tenancy setup done!")
+    logger.info("[Finished] Tenancy setup done!")
